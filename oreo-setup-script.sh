1c1e925c9c2954ea0c761645f9ca98e28c3fe251
IO-Shield <isaacoren42@gmail.com>
2020-07-24 05:28:17 +0100

    clean-up_conf's

--- oreo-setup-script.sh
+++ oreo-setup-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # run as root or with sudo perms
-cd /opt/
+cd ~/
 git clone https://github.com/OreoByte/another_omt.git
-chmod +x /opt/another_omt/thm_vpn
-chmod +x /opt/another_omt/curl_thm_vpn
-cp /opt/another_omt/tmux.conf-byte ~/.tmux.conf
+chmod +x ~/another_omt/thm_vpn
+chmod +x ~/another_omt/curl_thm_vpn
+cp ~/another_omt/tmux.conf-byte ~/.tmux.conf
